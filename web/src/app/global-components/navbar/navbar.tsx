df311fce95f4e9db407e59bb33110b40bbfb5491
pablosecuen <pablosecuen@gmail.com>
2024-03-19 14:41:14 -0300

    navbar mutation effect for mobile

--- web/src/app/global-components/navbar/navbar.tsx
+++ web/src/app/global-components/navbar/navbar.tsx
@@ -1,9 +1,14 @@
 /* eslint-disable @next/next/no-img-element */
-
+"use client";
 import Link from "next/link";
 import SocialMediaDisplay from "../icons/social-media-icons/social-media-display";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
   return (
     <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
@@ -17,14 +22,15 @@ const Navbar = () => {
             Logo
           </span>
         </Link>
-        <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+        <div className="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
           <SocialMediaDisplay />
           <button
+            onClick={toggleMenu}
             data-collapse-toggle="navbar-sticky"
             type="button"
             className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
             aria-controls="navbar-sticky"
-            aria-expanded="false"
+            aria-expanded={menuOpen ? "true" : "false"}
           >
             <span className="sr-only">Open main menu</span>
             <svg
@@ -45,7 +51,9 @@ const Navbar = () => {
           </button>
         </div>
         <div
-          className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+          className={`items-center justify-between w-full md:flex md:w-auto md:order-1 transition duration-500 overflow-hidden ${
+            menuOpen ? "opacity-1 h-full" : "opacity-0 h-0"
+          }`}
           id="navbar-sticky"
         >
           <ul className="flex flex-col text-sm p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
